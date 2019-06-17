8fa2f38334092ff6fe160a72da8c3a72b20cdbd2
Alisha Lopes <alishalopes@MacBook-Pro-5.home>
2019-06-17 15:22:25 -0700

    added model.py

---
+++ model.py
@@ -0,0 +1,57 @@
+from flask_sqlalchemy import SQLAlchemy
+
+
+db = SQLAlchemy()
+
+class Vendors(db.Model):
+
+	__tablename__ = "vendors"
+
+	vendor_id = db.Column(db.Integer, primary_key=True)
+	vendor_name = db.Column(db.Text)
+	billing_address = db.Column(db.Text)
+	account_number = db.Column(db.String)
+
+class Payment(db.Model):
+
+	__tablename__ = "payments"
+
+	payment_id = db.Column(db.Integer, primary_key=True)
+	amount = db.Column(db.Integer)
+	date = db.Column(db.Date)
+	payment_id = db.Column(db.Integer, db.ForeignKey('payment_type.payment_id'))
+
+class Payment_type(db.Model):
+
+	__tablename__ = "payment_types"
+
+	payment_id = db.Column(db.Integer, primary_key=True)
+	details = db.Column(db.Text)
+
+class Invoice(db.Model):
+
+	__tablename__ = "invoice"
+
+	invoice_id = db.Column(db.Integer, primary_key=True)
+	invoice_number = db.Column(db.Text)
+	amount = db.Column(db.Integer)
+	vendor_id = db.Column(db.Integer, db.ForeignKey('vendors.vendor_id'))
+	payment_id = db.Column(db.Integer, db.ForeignKey('payments.payment_id'))
+
+def connect_to_db(app):
+    """Connect the database to our Flask app."""
+
+    # Configure to use our PstgreSQL database
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///invoice'
+    app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True
+    db.app = app
+    db.init_app(app)
+
+
+if __name__ == "__main__":
+    # As a convenience, if we run this module interactively, it will leave
+    # you in a state of being able to work with the database directly.
+
+    from server import app
+    connect_to_db(app)
+    print("Connected to DB.")
