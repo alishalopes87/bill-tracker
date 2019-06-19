3473f70f8b72998befa46acd3d8d991b5756cfd9
vagrant <vagrant@vagrant.vm>
2019-06-19 21:19:06 +0000

    Added templates

---
+++ server.py
@@ -0,0 +1,27 @@
+from flask import Flask 
+from model import *
+
+app = Flask(__name__)
+
+
+@app.route('/')
+def get_vendors():
+
+	vendors = Vendor.query.all()
+
+	return render_template('vendors.html', vendors=vendors)
+
+
+if __name__ == "__main__":
+    # We have to set debug=True here, since it has to be True at the
+    # point that we invoke the DebugToolbarExtension
+    app.debug = False
+    # make sure templates, etc. are not cached in debug mode
+    app.jinja_env.auto_reload = app.debug
+
+    connect_to_db(app)
+
+    # Use the DebugToolbar
+    DebugToolbarExtension(app)
+
+    app.run(port=5000, host='0.0.0.0')
